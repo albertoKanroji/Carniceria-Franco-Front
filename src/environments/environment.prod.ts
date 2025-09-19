495bf05c2f46a93bda0163bbf6cfd4f10a47f611
Alberto de Jesús Hernández <129592318+albertoKanroji@users.noreply.github.com>
2025-09-19 09:00:03 -0600

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://infinity-gym-backend-app-0d84c543cb2b.herokuapp.com/api/v1'
+  apiUrl: 'https://infinity-tech-d98d94711f79.herokuapp.com/api/v1'
 };
